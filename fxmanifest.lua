49b7ce55dcd4e0c9ebf7f6c9ebad32313729b160
LXFSService <duisburgtaylor@icloud.com>
2025-12-13 00:12:46 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+game "gta5"
+version "1.0"
+
+author "LXFS - Service"
+description "LXFS-Loadingscreen"
+
+files {
+    "html/*.*",
+    "html/song.mp3"
+}
+
+loadscreen_cursor("yes")
+loadscreen "html/index.html"
